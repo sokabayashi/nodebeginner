ba4bc5e112acb3313469d9b41ed4541d7d18dbf8
Sai Okabayashi <saipuck@gmail.com>
2014-07-14 17:18:05 -0500

    book complete.  upload image.

--- router.js
+++ router.js
@@ -2,14 +2,15 @@
  * Created by saipuck on 7/14/14.
  */
 
-function route( handle, pathname, response, postData ) {
+function route( handle, pathname, response, request ) {
   console.log( "About to route a request for " + pathname );
 
   if( typeof handle[ pathname ] === 'function' ) { // if pathname is one we've defined
-    return handle[ pathname ]( response, postData );  // call that fn!
+    return handle[ pathname ]( response, request );  // call that fn!
   } else {
-    response.writeHead( 404, { 'Content-Type': 'text/plain'});
-    response.write( "No request handler found for " + pathname );
+    console.log( "No request handler found for " + pathname );
+    response.writeHead( 404, { 'Content-Type': 'text/html'});
+    response.write("404 Not found");
     response.end();
   }
 }
