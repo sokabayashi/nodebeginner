342e76e9e173d1373cdfceb015e5da19ba2639b5
Sai Okabayashi <saipuck@gmail.com>
2014-07-14 15:15:59 -0500

    through p 33.  'response' passed to request handlers.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,26 @@
 /**
  * Created by saipuck on 7/14/14.
  */
+var exec = require( "child_process" ).exec;
 
-function start() {
+function start( response ) {
   console.log( "Request handler 'start' was called." );
+
+  exec( "find /",
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function( error, stdout, stderr ) {
+    response.writeHead( 200, {'Content-Type': 'text/plain'} );
+    response.write( stdout );
+    response.end();
+  });
 }
 
-function upload() {
+function upload( response ) {
   console.log( "Request handler 'upload' was called." );
+  response.writeHead( 200, {'Content-Type': 'text/plain'});
+  response.write( 'Hello Upload' );
+  response.end();
+
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -2,13 +2,15 @@
  * Created by saipuck on 7/14/14.
  */
 
-function route( handle, pathname ) {
+function route( handle, pathname, response ) {
   console.log( "About to route a request for " + pathname );
 
-  if( typeof handle[ pathname ] === 'function' ) {
-    handle[ pathname ]();
+  if( typeof handle[ pathname ] === 'function' ) { // if pathname is one we've defined
+    return handle[ pathname ]( response );  // call that fn!
   } else {
-    console.log( "No request handler found for " + pathname );
+    response.writeHead( 404, { 'Content-Type': 'text/plain'});
+    response.write( "No request handler found for " + pathname );
+    response.end();
   }
 }
 
